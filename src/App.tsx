63751d9d3ee1759f665be1ee85c0f28e9b80dde5
Amogh Krishna <amoghkrishna55@gmail.com>
2023-12-24 16:48:04 +0530

    Add routing and react-query to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const base = "/AdoptMe";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter basename={base}>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
